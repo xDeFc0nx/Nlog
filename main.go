51887265ad728823dfa569212f6ba5962b744eb0
Nehar <nehartale8@gmail.com>
2025-12-05 03:12:42 +0100

    feat: Implement logging functionalities

---
+++ main.go
@@ -0,0 +1,74 @@
+package nlog
+
+import (
+	"fmt"
+	"maps"
+)
+
+type Log struct {
+	Level string `json:"level"`
+	Msg   string `json:"msg"`
+	Trace string `json:"trace"`
+}
+
+func Info(msg string, context map[string]any) {
+	logEntry := Log{
+		Level: "INFO",
+		Msg:   msg,
+	}
+
+	data := map[string]any{
+		"level": logEntry.Level,
+		"msg":   logEntry.Msg,
+	}
+
+	if context != nil {
+		maps.Copy(data, context)
+	}
+
+	fmt.Println(Prettify(data))
+}
+
+func Warn(msg string, context map[string]any) {
+	logEntry := Log{
+		Level: "WARN",
+		Msg:   msg,
+	}
+
+	data := map[string]any{
+		"level": logEntry.Level,
+		"msg":   logEntry.Msg,
+	}
+
+	if context != nil {
+		maps.Copy(data, context)
+	}
+
+	fmt.Println(Prettify(data))
+}
+func Error(err error, context map[string]any) {
+	if err == nil {
+		return
+	}
+
+	file, fn, line := GetTrace(2)
+	traceString := fmt.Sprintf("%s:%d  %s", file, line, fn)
+
+	logEntry := Log{
+		Level: "ERROR",
+		Msg:   err.Error(),
+		Trace: traceString,
+	}
+
+	data := map[string]any{
+		"level": logEntry.Level,
+		"msg":   logEntry.Msg,
+		"trace": logEntry.Trace,
+	}
+
+	if context != nil {
+		maps.Copy(data, context)
+	}
+
+	fmt.Println(Prettify(data))
+}
